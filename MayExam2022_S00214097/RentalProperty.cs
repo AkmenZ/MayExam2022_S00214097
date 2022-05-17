83dcc458e694493978bb8c920ffa545dba542748
Janis Akmentins <akmenz89@gmail.com>
2022-05-17 09:52:28 +0100

    UnitTest added, RentalProperty class added

---
+++ MayExam2022_S00214097/RentalProperty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayExam2022_S00214097
+{
+    public class RentalProperty
+    {
+        //properties
+        public int ID { get; set; }
+        public Enum RentalType { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        //constructors
+
+        //methods
+        public void RentIncrease(int increase)
+        {
+            Price = Price + ((increase / 100) * Price);
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MayExam2022_S00214097;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_RentIncrease()
+        {
+            //arrange
+            RentalProperty r1 = new RentalProperty() { Price = 300m};
+            decimal expectedPrice = 330m;
+
+            //act
+            r1.RentIncrease(10);
+
+            //assert
+            Assert.AreEqual(expectedPrice, r1.Price);
+        }
+    }
+}
